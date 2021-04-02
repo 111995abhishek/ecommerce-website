c7d43cd3882266f068a57230c47ac75506d80211
abhishek <111995abhishek@gmail.com>
2021-04-02 19:36:49 +0530

    added image in product category

--- src/components/Category/index.js
+++ src/components/Category/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import garden from './garden.jpg';
+import spritual from './spritual.jpeg';
 
 const ProductCategory = props => {
     return (
-        <div>
-            <p>product category will be displayed here</p>
+        <div className="productcategory">
+            <div className="wrap">
+                <div className="item" style={{backgroundImage: `url(${spritual})`}}>
+                    <a>Shop Spritual Products</a>
+                </div>
+
+                <div className="item" style={{backgroundImage: `url(${garden})`}}>
+                    <a>Shop Graden Products</a>
+                </div>
+
+            </div>
+            
         </div>
     );
 }
