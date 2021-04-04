6fe24c2eb37ca044803b0720d02dc76aff51117d
abhishek <111995abhishek@gmail.com>
2021-04-05 02:30:18 +0530

    created new component called register and added route path for homepage and register

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import React from 'react';
 import Header from './components/header';
 import  './default.scss';
 import HomePage from './Pages/homepage/HomePage';
+import Register from './Pages/register';
+import {Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <div className="main">
-        <HomePage/>
+
+        <Route exact path='/' component={HomePage}>
+          <HomePage/>
+        </Route>
+        <Route  exact path='/register' component={Register}>
+          <Register/>
+        </Route>
       </div>
+      
       
     </div>
   );
---
+++ src/Pages/register/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Register extends Component {
+    render() {
+        return (
+            <div>
+                <h3>register component is under construction</h3>   
+            </div>
+        );
+    }
+}
+
+export default Register ;
